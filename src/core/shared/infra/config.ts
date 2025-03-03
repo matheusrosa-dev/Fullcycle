4093122981deb39827e20d8bcc246a2aa5709318
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2025-03-03 20:30:18 -0300

    add nest

--- src/core/shared/infra/config.ts
+++ src/core/shared/infra/config.ts
@@ -21,7 +21,7 @@ export class Config {
     }
 
     Config.env = readEnv({
-      path: join(__dirname, `../../../envs/.env.${process.env.NODE_ENV}`),
+      path: join(__dirname, `../../../../envs/.env.${process.env.NODE_ENV}`),
     }).parsed;
   }
 }
